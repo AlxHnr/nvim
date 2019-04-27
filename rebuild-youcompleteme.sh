dcd86d9d9238989be2008fb404ff2d9ab339a226
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-04-27 16:36:18 +0200

    Change YouCompleteMe cmake flags

--- rebuild-youcompleteme.sh
+++ rebuild-youcompleteme.sh
@@ -5,6 +5,6 @@ trap 'rm -rf "$ycm_rebuild_dir"' EXIT
 
 mkdir -p "$ycm_rebuild_dir"
 cd "$ycm_rebuild_dir"
-cmake -DEXTERNAL_LIBCLANG_PATH=/usr/lib64/libclang.so.7 \
+cmake -DUSE_SYSTEM_LIBCLANG=ON \
   "$HOME/.config/nvim/plugged/YouCompleteMe/third_party/ycmd/cpp/"
 make "-j$(nproc)"
