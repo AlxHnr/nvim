ddfdf9a52ed0b3633059c77408149bea0ad9bc0d
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-10-27 15:12:26 +0100

    Add script for updating everything

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# This script updates everything related to this neovim setup.
+
+cd "$(dirname "$0")"
+
+git pull
+pip3 install --user neovim --upgrade
+./update-bindir.sh
+
+./bin/nvim +PlugUpdate +qall
+
+echo "SUCCESS!"
