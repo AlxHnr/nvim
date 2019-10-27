e93f60a9b80f4e2885f20fbd13d4288295f136b2
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-10-27 14:47:52 +0100

    Add script for populating bin/ with Linux binaries

---
+++ update-bindir.sh
@@ -0,0 +1,60 @@
+#!/bin/sh -e
+
+# This script populates and updates the bin/ directory with 64-Bit linux
+# binaries.
+
+check_hash()
+(
+  filename="$1"
+  sha256="$2"
+
+  printf "%s %s\n" "$sha256" "$filename" | sha256sum -c -
+)
+
+fetch()
+(
+  process_download_callback="$1"
+  filename="$2"
+  sha256="$3"
+  url="$4"
+
+  if check_hash "$filename" "$sha256" >/dev/null 2>&1; then
+    printf "Already up-to-date: %s\n" "$filename"
+    return
+  fi
+
+  printf "Fetching %s...\n" "$filename"
+  trap 'rm -f .tmp_download' EXIT
+  wget --show-progress --quiet "$url" -O .tmp_download
+
+  "$process_download_callback" .tmp_download "$filename" "$sha256"
+  chmod +x "$filename"
+
+  printf "SUCCESS: %s\n" "$filename"
+)
+
+and_rename()
+(
+  download="$1"
+  filename="$2"
+  sha256="$3"
+
+  check_hash "$download" "$sha256"
+  mv "$download" "$filename"
+)
+
+and_extract()
+(
+  archive="$1"
+  filename="$2"
+  sha256="$3"
+
+  trap 'rm -f ".tmp__$filename"' EXIT
+  tar xaf "$archive" "$filename" -O > ".tmp__$filename"
+  check_hash ".tmp__$filename" "$sha256"
+
+  mv ".tmp__$filename" "$filename"
+)
+
+mkdir -p "$HOME/.config/nvim/bin"
+cd "$HOME/.config/nvim/bin"
