84449ccfa49cadd699fad5189a463624a843a854
Alexander Heinrich <alxhnr@users.noreply.github.com>
2019-12-10 22:57:19 +0100

    Update texlab to 1.8.0

--- update-bindir.sh
+++ update-bindir.sh
@@ -62,5 +62,5 @@ fetch and_rename nvim a3b7ccbde583acdc7f3385025d3c5e386f92aa97e425b311a494028369
   "https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage"
 fetch and_extract fzf cc0a4a04ec7d892d68ebe4db7c70c0b4130848b9a17f99b02b254238f7db6a44 \
   "https://github.com/junegunn/fzf-bin/releases/download/0.19.0/fzf-0.19.0-linux_amd64.tgz"
-fetch and_extract texlab 87d35026d6be6c25b376f1c2d61dd93f43a4c9ff071c51b72ecf0e3a39e3a933 \
-  "https://github.com/latex-lsp/texlab/releases/download/v1.7.0/texlab-x86_64-linux.tar.gz"
+fetch and_extract texlab 06db4deea377da7fe6beca97f266f63ef35e36367d62371812fd153744d6d138 \
+  "https://github.com/latex-lsp/texlab/releases/download/v1.8.0/texlab-x86_64-linux.tar.gz"
