b3780155a82012b246c5618392e909e97036c0dc
Alexander Heinrich <alxhnr@users.noreply.github.com>
2020-03-15 12:52:23 +0100

    Update fzf to 0.21.0

--- update-bindir.sh
+++ update-bindir.sh
@@ -60,7 +60,7 @@ cd bin/
 
 fetch and_rename nvim a3b7ccbde583acdc7f3385025d3c5e386f92aa97e425b311a49402836998b4e0 \
   "https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage"
-fetch and_extract fzf 29dfb84ef14dd6dd78df1dbf7624048607c6878e731b1e84713f6469d6f9081f \
-  "https://github.com/junegunn/fzf-bin/releases/download/0.20.0/fzf-0.20.0-linux_amd64.tgz"
+fetch and_extract fzf 87b12136842fd5c09971fd4813a8e918fc4cc30850953f346814d4174328cae8 \
+  "https://github.com/junegunn/fzf-bin/releases/download/0.21.0/fzf-0.21.0-linux_amd64.tgz"
 fetch and_extract texlab 04324f122ef8a8a2a3ba2568de0ed7fb3646f592d00181c4abb1a73c81c6bd6d \
   "https://github.com/latex-lsp/texlab/releases/download/v1.10.0/texlab-x86_64-linux.tar.gz"
